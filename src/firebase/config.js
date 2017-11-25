3d20c815a712cc045433a8197b089f453fdb6c6b
d3ming <d3ming@gmail.com>
2017-11-24 21:35:19 -0800

    Update with my config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,8 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyBsVVpEDrlNPEmshLcmOuE0FxhjPn0AqMg',
-  authDomain: 'todo-react-redux.firebaseapp.com',
-  databaseURL: 'https://todo-react-redux.firebaseio.com',
-  storageBucket: 'firebase-todo-react-redux.appspot.com'
+  apiKey: "AIzaSyC5BJA8xxl7Jqv4Qf42mQN-suqJTzCf8yg",
+  authDomain: "dm-todo-8a516.firebaseapp.com",
+  databaseURL: "https://dm-todo-8a516.firebaseio.com",
+  projectId: "dm-todo-8a516",
+  storageBucket: "dm-todo-8a516.appspot.com",
+  messagingSenderId: "1073490852522"
 };
